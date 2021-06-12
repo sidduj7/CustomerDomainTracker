cb2a477a93f7a596ac324577a4dabb4db10ad8d7
siddhardh_jakkampudi <siddhardh_jakkampudi@II39-PF1VC5HX.mhf2.mhf.mhc>
2021-06-12 22:49:49 +0530

    initial commit

---
+++ src/main/java/com/springboot/customer/CustomerDomainTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerDomainTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerDomainTrackerApplication.class, args);
+	}
+
+}
